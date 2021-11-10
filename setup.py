e68d5f816676a9b949390adeb664d69476388635
Guntis Liepins <turbiina@gmail.com>
2021-11-10 21:06:39 +0200

    Fix SyntaxError: keyword argument repeated in setup.py

--- setup.py
+++ setup.py
@@ -13,8 +13,7 @@ classifiers=[
           'Programming Language :: Python :: 3',
           ]
 
-setup(name='Distutils',
-      name='nutanixapi',
+setup(name='nutanixapi',
       version='0.1',
       description='Simplified Nutanix API library',
       lond_description="Simplified Nutanix API library",
